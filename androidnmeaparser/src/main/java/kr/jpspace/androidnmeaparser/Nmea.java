a3f2976f06622ac1bb5a2b869d3c0671b31a604a
Taeil Kim <kimtis3168@gmail.com>
2016-08-25 10:13:13 +0900

    Test

--- androidnmeaparser/src/main/java/kr/jpspace/androidnmeaparser/Nmea.java
+++ androidnmeaparser/src/main/java/kr/jpspace/androidnmeaparser/Nmea.java
@@ -1,8 +1,14 @@
 package kr.jpspace.androidnmeaparser;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.widget.Toast;
+
 /**
  * Created by kimti on 2016-08-25.
  */
 public class Nmea {
-    
+    public void showToast(Context context, Resources resources) {
+        Toast.makeText(context, resources.getString(R.string.app_name), Toast.LENGTH_LONG).show();
+    }
 }
--- sample/src/main/java/com/kimtis/androidnmeaparser/MainActivity.java
+++ sample/src/main/java/com/kimtis/androidnmeaparser/MainActivity.java
@@ -3,11 +3,16 @@ package com.kimtis.androidnmeaparser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.jpspace.androidnmeaparser.Nmea;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Nmea nmea = new Nmea();
+        nmea.showToast(this, getResources());
     }
 }
