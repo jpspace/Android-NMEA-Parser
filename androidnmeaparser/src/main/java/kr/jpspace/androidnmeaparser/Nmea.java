3b94aeb63cfdeb2b0f1ccdf7234acc37517cf6e5
UiSeok <uskim3898@gmail.com>
2016-08-25 11:03:44 +0900

    nmea constructor made contstructor made

--- androidnmeaparser/src/main/java/kr/jpspace/androidnmeaparser/Nmea.java
+++ androidnmeaparser/src/main/java/kr/jpspace/androidnmeaparser/Nmea.java
@@ -1,8 +1,31 @@
 package kr.jpspace.androidnmeaparser;
 
 /**
- * Created by kimti on 2016-08-25.
+ *
  */
 public class Nmea {
-    
+    private String GPGGA = "";
+    private String GPGSV = "";
+    private String GPRMC = "";
+    private String GPGSA = "";
+
+    public Nmea(String nmea) {
+        String[] nmeaArray = nmea.split("\r\n");
+        for (String s : nmeaArray) {
+            switch (s.substring(1, 6)) {
+                case "GPGGA":
+                    GPGGA += s + "\n";
+                    break;
+                case "GPGSV":
+                    GPGSV += s + "\n";
+                    break;
+                case "GPRMC":
+                    GPRMC += s + "\n";
+                    break;
+                case "GPGSA":
+                    GPGSA += s + "\n";
+                    break;
+            }
+        }
+    }
 }
